c2270fe7b70b6040cd6cb734730d2f7d89f691d8
Maximilian Hess <mail@maximilianhess.com>
2019-06-07 20:44:49 +0200

    Split deleration and implementation.

---
+++ gamepad.cpp
@@ -0,0 +1,65 @@
+#include "gamepad.hpp"
+
+namespace gamepad {
+
+Gamepad::Gamepad() : m_quit(false) {
+	gamepad::NoGamepadsFoundException ex;
+
+    SDL_Init(SDL_INIT_JOYSTICK);
+    const int num_joysticks = SDL_NumJoysticks();
+    if (num_joysticks < 1) {
+		throw ex;
+    }
+
+    // pick the first gamepad having more than two axis
+    bool found = false;
+    for (unsigned int i = 0; i < num_joysticks; i++) {
+        m_gamepad = SDL_JoystickOpen(i);
+        if (SDL_JoystickNumAxes(m_gamepad) > 2) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        throw ex;
+    }
+
+    SDL_JoystickEventState(SDL_ENABLE);
+    SDL_JoystickEventState(SDL_QUERY);
+
+    std::thread(&Gamepad::updateLoop, this).detach();
+}
+
+void Gamepad::getEvent(Event &gamepadEvent) {
+    SDL_Event event;
+    std::vector<bool>  buttons;
+    std::vector<int> axis;
+
+    SDL_JoystickUpdate();
+    
+    // get keys
+    for (int i = 0; i < SDL_JoystickNumButtons(m_gamepad); i++) {
+        unsigned int n = SDL_JoystickGetButton(m_gamepad, i);
+        buttons.push_back((n == 1) ? true : false);
+    }
+    
+    // get axis
+    for (int i = 0; i < SDL_JoystickNumAxes(m_gamepad); i++) {
+        int a = SDL_JoystickGetAxis(m_gamepad, i);
+        axis.push_back(a);
+    }
+
+    gamepadEvent.setButtons(buttons);
+    gamepadEvent.setAxis(axis);
+}
+
+void Gamepad::updateLoop() {
+    while (!m_quit) {
+        gamepad::Event event;
+        getEvent(event);
+        m_ui.update(event);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+}
+
+}
--- gamepad.hpp
+++ gamepad.hpp
@@ -14,7 +14,7 @@ class NoGamepadsFoundException : public std::exception {
     virtual const char* what() const throw() {
         return "failed to find any gamepad";
     }
-} NoGamepadsFoundException;
+};
 
 class Gamepad {
 
@@ -23,65 +23,12 @@ private:
     bool m_quit;
     UI m_ui;
 
-    void getEvent(Event &gamepadEvent) {
-        SDL_Event event;
-        std::vector<bool>  buttons;
-        std::vector<int> axis;
+    void getEvent(Event &gamepadEvent);
 
-        SDL_JoystickUpdate();
-        
-        // get keys
-        for (int i = 0; i < SDL_JoystickNumButtons(m_gamepad); i++) {
-            unsigned int n = SDL_JoystickGetButton(m_gamepad, i);
-            buttons.push_back((n == 1) ? true : false);
-        }
-        
-        // get axis
-        for (int i = 0; i < SDL_JoystickNumAxes(m_gamepad); i++) {
-            int a = SDL_JoystickGetAxis(m_gamepad, i);
-            axis.push_back(a);
-        }
-
-        gamepadEvent.setButtons(buttons);
-        gamepadEvent.setAxis(axis);
-    }
-
-    void updateLoop() {
-        while (!m_quit) {
-            gamepad::Event event;
-            getEvent(event);
-            m_ui.update(event);
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        }
-    }
+    void updateLoop();
 
 public:
-    Gamepad() : m_quit(false) {
-        SDL_Init(SDL_INIT_JOYSTICK);
-        const int num_joysticks = SDL_NumJoysticks();
-        if (num_joysticks < 1) {
-            throw NoGamepadsFoundException;
-        }
-
-        // pick the first gamepad having more than two axis
-        bool found = false;
-        for (unsigned int i = 0; i < num_joysticks; i++) {
-            m_gamepad = SDL_JoystickOpen(i);
-            if (SDL_JoystickNumAxes(m_gamepad) > 2) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            throw NoGamepadsFoundException;
-        }
-
-        SDL_JoystickEventState(SDL_ENABLE);
-        SDL_JoystickEventState(SDL_QUERY);
-
-        std::thread(&Gamepad::updateLoop, this).detach();
-    }
-
+    Gamepad();
 };
 
 }
